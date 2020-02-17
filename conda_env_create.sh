0dbd88260590644554b7356dcccfb16598e0bb18
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2020-02-17 15:03:46 -0700

    make conda channel strict

--- conda_env_create.sh
+++ conda_env_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda create -n wrf_subset_regrid -c conda-forge -y \
+conda create -n wrf_subset_regrid -c conda-forge --strict -y \
     python=3.7 \
     esmpy=7.1.0 \
     numpy xesmf \
